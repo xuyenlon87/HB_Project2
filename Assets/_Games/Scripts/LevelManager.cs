3c2419ceee77e77fde6c95df3fe25fc00badcd61
Linh Đặng Vũ <xuyenlon87@gmail.com>
2023-12-25 23:36:41 +0700

    23h

--- Assets/_Games/Scripts/LevelManager.cs
+++ Assets/_Games/Scripts/LevelManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    private int col = 10;
-    private int row = 10;
+    private int col1 = 10;
+    private int row1 = 10;
+    private int sum1 =0;
     public int currentMap = 0;
 
     private static LevelManager instance;
@@ -51,12 +52,20 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         DrawMap();
+        SpawmPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void SpawmPlayer()
+    {
+        var xPos = Random.Range(1, 8);
+        var zPos = Random.Range(1, 8);
+        Instantiate(player, new Vector3(xPos, 1.5f, zPos), Quaternion.identity);
     }
     public void DrawMap()
     {
@@ -68,85 +77,36 @@ public class LevelManager : MonoBehaviour
             }
             listBrickDelete.Clear();
         }
-        //if (currentMap >= 0 && currentMap < testMap.Count)
-        //{
-        //    string data = testMap[currentMap].text;
-        //    Debug.Log(currentMap);
-        //    string[] dataArray = data.Split("\r\n");
-        //    int row = dataArray.Length;
-        //    int col = dataArray[0].Split(",").Length;
-        //    int[,] map = new int[row, col];
-        //    for (int i = 0; i < row; i++)
-        //    {
-        //        if (dataArray.Length > 0)
-        //        {
-        //            string[] dataArray2 = dataArray[i].Split(",");
-        //            for (int j = 0; j < dataArray2.Length; j++)
-        //            {
-        //                map[i, j] = int.Parse(dataArray2[j]);
-        //                if (map[i, j] == 2)
-        //                {
-        //                    GameObject brick = Instantiate(brickPrefabGreen, new Vector3(j, 0.5f, -i), Quaternion.identity);
-        //                    listBrickDelete.Add(brick);
-        //                }
-        //                else if (map[i, j] == 1)
-        //                {
-        //                    GameObject brickStart = Instantiate(brickPrefabBlack, new Vector3(j, 0.5f, -i), Quaternion.identity);
-        //                    listBrickDelete.Add(brickStart);
-        //                    //player.transform.position = brickStart.transform.position + Vector3.up;
-        //                }
-        //                else if (map[i, j] == 3)
-        //                {
-        //                    GameObject brick = Instantiate(brickPrefabRed, new Vector3(j, 0.5f, -i), Quaternion.identity);
-        //                    listBrickDelete.Add(brick);
-        //                }
-        //                else if (map[i, j] == 0)
-        //                {
-        //                    GameObject brick = Instantiate(brickGround, new Vector3(j, 0.5f, -i), Quaternion.identity);
-        //                    listBrickDelete.Add(brick);
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-        for (int i = 0; i < col; i++)
+        for (int i = 0; i < col1; i++)
         {
-            for (int j = 0; j < row; j++)
+            for (int j = 0; j < row1; j++)
             {
+                
                 int color = Random.Range(0, 4);
                 if(color == 0 && countBlue <=25)
                 {
                     countBlue += 1;
                     Instantiate(brickPrefabBlue, new Vector3(i, 0.5f, j), Quaternion.identity);
+                    listBrickDelete.Add(brickPrefabBlue);
                 }
-                if (color == 1 && countRed <=25)
+                else if (color == 1 && countRed <=25)
                 {
                     countRed += 1;
                     Instantiate(brickPrefabRed, new Vector3(i, 0.5f, j), Quaternion.identity);
+                    listBrickDelete.Add(brickPrefabRed);
                 }
-                if (color == 2 && countGreen <=25)
+                else if (color == 2 && countGreen <=25)
                 {
                     countGreen += 1;
                     Instantiate(brickPrefabGreen, new Vector3(i, 0.5f, j), Quaternion.identity);
+                    listBrickDelete.Add(brickPrefabGreen);
                 }
-                if (color == 3 && countYellow <=25)
+                else if (color == 3 && countYellow <=25)
                 {
                     countYellow += 1;
                     Instantiate(brickPrefabYellow, new Vector3(i, 0.5f, j), Quaternion.identity);
+                    listBrickDelete.Add(brickPrefabYellow);
                 }
-                if(countBlue == 25)
-                {
-                    color = Random.Range(1, 4);
-                }
-                if (countRed == 25)
-                {
-                    color = Random.Range(2, 4);
-                }
-                if (countGreen == 25)
-                {
-                    color = Random.Range(3, 4);
-                }
-
             }
         }
     }

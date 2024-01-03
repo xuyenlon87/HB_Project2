a382148d9de74af11bcbe384755e268dbb391907
Linh Đặng Vũ <xuyenlon87@gmail.com>
2024-01-04 00:03:43 +0700

    0h

--- Assets/_Games/Scripts/Player.cs
+++ Assets/_Games/Scripts/Player.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] GameObject back;
-    [SerializeField] List<GameObject> brickOnPlayer;
     [SerializeField] LayerMask ground;
     [SerializeField] LayerMask bridge;
     [SerializeField] private Material ColorBrick;
+    [SerializeField] private List<GameObject> listBackBrick = new List<GameObject>();
 
     public float speed;
     public FixedJoystick fixedJoystick;
     public Rigidbody rb;
 
-    private List<GameObject> listBackBrick = new List<GameObject>();
-    private Renderer rend;
     private RaycastHit hit;
-    private Vector3 targetPos;
-    private int i = 1;
-
-
     private Vector3 posYBack = Vector3.zero;
     // Start is called before the first frame update
     void Start()
@@ -31,36 +25,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 10f, bridge))
+        Vector3 direction = Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
+        Vector3 targetPos = transform.position + transform.forward * 0.5f;
+        if (Physics.Raycast(targetPos, Vector3.down, out hit, 10f, bridge))
         {
-            Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.black, 10f);
-            if (hit.collider.GetComponent<Renderer>().material != ColorBrick)
+            Renderer renderer = hit.collider.GetComponent<Renderer>();
+            Debug.DrawRay(targetPos, Vector3.down * hit.distance, Color.black, 10f);
+            if (renderer.material != ColorBrick)
             {
                 if (listBackBrick.Count > 0)
                 {
-                    hit.collider.GetComponent<Renderer>().material = ColorBrick;
-                    Debug.Log(hit.collider.GetComponent<Renderer>().material);
-                }
-                else
-                {
-                    rb.velocity = Vector3.zero;
+                    renderer.material = ColorBrick;
+                    Debug.Log(renderer);
                 }
             }
-            Debug.DrawRay(targetPos, Vector3.down * hit.distance, Color.black, 10f);
-            Debug.Log("Did Hit");
         }
+
+        rb.velocity = direction * speed;
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
     }
     public void FixedUpdate()
     {
-        Vector3 direction = Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;       
-        rb.velocity = direction * speed;
-        transform.rotation = Quaternion.LookRotation(rb.velocity);
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("BlueBrick"))
         {
-            brickOnPlayer.Add(other.gameObject);
             LevelManager.Instance.listSpawmBrick.Add(other.transform.position);
             listBackBrick.Add(other.gameObject);
             other.transform.parent = back.transform;
@@ -68,6 +59,21 @@ public class Player : MonoBehaviour
             other.transform.localPosition = new Vector3(0, posYBack.y , 0);
             posYBack.y += 0.1f;
             other.GetComponent<Renderer>().material = ColorBrick;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BridgeBrick"))
+        {
+            Renderer renderer = collision.collider.GetComponent<Renderer>();
+            if (renderer != ColorBrick)
+            {
+                Destroy(listBackBrick[listBackBrick.Count - 1]);
+                listBackBrick.RemoveAt(listBackBrick.Count - 1);
+                posYBack.y -= 0.1f;
+            }
         }
     }
 }

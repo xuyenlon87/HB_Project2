7aaa9ad3f67b10d0787c52308fc02836a645e1a1
Linh Đặng Vũ <xuyenlon87@gmail.com>
2024-01-08 15:59:59 +0700

    16h

--- Assets/_Games/Scripts/Player.cs
+++ Assets/_Games/Scripts/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour
     [SerializeField] LayerMask bridge;
     [SerializeField] private Material ColorBrick;
     [SerializeField] private List<GameObject> listBackBrick = new List<GameObject>();
-    [SerializeField] private bool canMove = true;
 
     public float speed;
     public FixedJoystick fixedJoystick;
@@ -17,6 +16,8 @@ public class Player : MonoBehaviour
 
     private RaycastHit hit;
     private Vector3 posYBack = Vector3.zero;
+    private Vector3 targetPos;
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,15 +27,45 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
-        Vector3 targetPos = transform.position + transform.forward * 0.5f;
-        rb.velocity = direction * speed;
-        transform.rotation = Quaternion.LookRotation(rb.velocity);
-        Debug.DrawRay(targetPos, Vector3.down * hit.distance, Color.black, 10f);
+        Move();
     }
     public void FixedUpdate()
     {
 
+
+    }
+
+    private void Move()
+    {
+        targetPos = transform.position + transform.forward * 0.5f;
+        Physics.Raycast(targetPos, Vector3.down, out hit, 10f);
+        if (hit.collider)
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Bridge"))
+            {
+                if (listBackBrick.Count <= 0)
+                {
+                    rb.velocity = Vector3.zero;
+                    targetPos = transform.position - transform.forward * 0.5f;
+                }
+            }
+            else
+            {
+                direction = Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal;
+                rb.velocity = direction * speed;
+            }
+        }
+        if (!hit.collider)
+        {
+            rb.velocity = Vector3.zero;
+            targetPos = transform.position - transform.forward * 0.5f;
+        }
+        if (rb.velocity.magnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        }
+        Debug.DrawRay(targetPos, Vector3.down * hit.distance, Color.black, 10f);
+        Debug.Log(direction);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -61,8 +92,11 @@ public class Player : MonoBehaviour
                 if (listBackBrick.Count > 0)
                 {
                     renderer.material = ColorBrick;
-                    Destroy(listBackBrick[listBackBrick.Count - 1]);
-                    listBackBrick.RemoveAt(listBackBrick.Count - 1);
+                    collision.gameObject.layer = LayerMask.NameToLayer("Ground");
+                    collision.gameObject.tag = "GroundBrick";
+                    var del = listBackBrick.Count - 1;
+                    Destroy(listBackBrick[del]);
+                    listBackBrick.RemoveAt(del);
                     posYBack.y -= 0.1f;
                 }
             }
